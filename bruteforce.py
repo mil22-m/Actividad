8695ba0e5b11480b1d8481d74384ae96208b7107
mil22-m <milenatacoll@gmail.com>
2025-09-20 18:08:20 -0500

    bruteforce.py
    m

---
+++ bruteforce.py
@@ -0,0 +1,31 @@
+import itertools
+import time
+
+password = "wzm"
+
+alfabeto = "abcdefghijklmnopqrstuvwxyz"
+
+intentos = 0
+tiempo_inicia = time.time()
+
+found = False
+max_length = 4  
+
+for a in range(1, max_length + 1):
+    
+    for b in itertools.product(alfabeto, repeat=a):
+        intentos += 1
+        intento = "".join(b)
+        if intento == password:
+            found = True
+            acaba_tiempo = time.time()
+            break
+    if found:
+        break
+
+if found:
+    print(f"contraseña encontrada")
+    print(f"numero de intentos: {intentos}")
+    print(f"Tiempo de: {acaba_tiempo - tiempo_inicia:.4f} segundos")
+else:
+    print(" Contraseña no encontrada")
